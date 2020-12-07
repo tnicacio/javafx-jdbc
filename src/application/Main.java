6655be660939e7e37e59fe9f567b5f649a529bc8
tnicacio <nicacio.t92@gmail.com>
2020-12-07 18:21:33 -0300

    MainView design

--- src/application/Main.java
+++ src/application/Main.java
@@ -6,7 +6,6 @@ import javafx.scene.Scene;
 import javafx.scene.control.ScrollPane;
 import javafx.stage.Stage;
 
-
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
